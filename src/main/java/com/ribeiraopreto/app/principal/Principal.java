9eda5f1727de6cac072f24a927c25cd717931093
Caio Belchior <caio.belchior@hotmail.com>
2024-09-03 08:39:22 -0300

    feat: verify number on fibonacci sequence

--- src/main/java/com/ribeiraopreto/app/principal/Principal.java
+++ src/main/java/com/ribeiraopreto/app/principal/Principal.java
@@ -9,8 +9,31 @@ public class Principal {
     private final FibonacciService fibonacciService = new FibonacciService();
 
     public void showMenu() {
-        System.out.println("Informe um número para saber se ele pertence à sequência de Fibonacci");
-        int number = scanner.nextInt();
-        boolean exist = fibonacciService.existsOnFibonacciSequence(number);
+        System.out.println("##### BOAS VINDAS AO CONSOLE DO TESTE PRATICO ESTAGIO - RIBEIRAO PRETO #####");
+        try {
+            int chosenItem = 0;
+            while (chosenItem != 5) {
+                showMenuItens();
+                String typedText = scanner.nextLine();
+                chosenItem = Integer.parseInt(typedText);
+
+                switch (chosenItem) {
+                    case 1 -> fibonacciService.verify();
+                    case 5 -> System.out.println("Saindo...");
+                    default -> {
+                        System.out.println("Opção selecionada inválida, tente novamente");
+                        chosenItem = 0;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showMenuItens() {
+        System.out.println("\nDIGITE O NÚMERO DA OPERAÇÃO DESEJADA:");
+        System.out.println("1 -> Verificar se número digitado pertence à sequência de Fibonacci");
+        System.out.println("5 -> Sair");
     }
 }
